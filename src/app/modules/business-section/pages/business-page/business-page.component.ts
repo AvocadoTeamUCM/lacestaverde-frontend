251caab143959fccb72cd41bbd327d75f83c04e2
Alejandro <alejleal@ucm.es>
2023-03-22 10:32:29 +0100

    Added map in business card

--- src/app/modules/business-section/pages/business-page/business-page.component.ts
+++ src/app/modules/business-section/pages/business-page/business-page.component.ts
@@ -7,10 +7,21 @@ import { Router } from '@angular/router';
   styleUrls: ['./business-page.component.scss'],
 })
 export class BusinessPageComponent  implements OnInit {
-
+  
   constructor(private router: Router) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    const mapboxgl = require('mapbox-gl');
+ 
+    mapboxgl.accessToken = 'pk.eyJ1IjoiYWxlamxlYWwiLCJhIjoiY2xmaWk2MzQwNGpqYzN5bnQ4MDFqeGJ2eSJ9.zaMfzrH3EN6hqtpiMIXaWw';
+    const map = new mapboxgl.Map({
+      container: 'map', // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    });
+  
+  }
 
 
   /************************************************************************** 
